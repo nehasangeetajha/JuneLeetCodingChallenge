7bafb3059874c0ec014ff6bd6964d39f8260f0d5
Sangeeta Jha <43818333+nehasangeetajha@users.noreply.github.com>
2020-06-22 11:00:42 +0530

    Create DungeonGame.cpp

---
+++ DungeonGame.cpp
@@ -0,0 +1,49 @@
+//https://leetcode.com/explore/featured/card/june-leetcoding-challenge/541/week-3-june-15th-june-21st/3367/
+/*
+The demons had captured the princess (P) and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight (K) was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
+
+The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+
+Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
+
+In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+
+ 
+
+Write a function to determine the knight's minimum initial health so that he is able to rescue the princess.
+
+For example, given the dungeon below, the initial health of the knight must be at least 7 if he follows the optimal path RIGHT-> RIGHT -> DOWN -> DOWN.
+
+-2 (K)	-3	3
+-5	-10	1
+10	30	-5 (P)
+ 
+
+Note:
+
+The knight's health has no upper bound.
+Any room can contain threats or power-ups, even the first room the knight enters and the bottom-right room where the princess is imprisoned.
+*/
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int r = dungeon.size();
+        if(r==0)
+            return 0;
+        int c = dungeon[0].size();
+        vector<vector<int>>dp(r+1,vector<int>(c+1,INT_MAX));
+        dp[r-1][c]=1;
+        dp[r][c-1]=1;
+        
+        for(int i = r-1;i>=0;i--)
+        {
+            for(int j=c-1;j>=0;j--)
+            {
+                int val = min(dp[i+1][j],dp[i][j+1]) - dungeon[i][j];
+                dp[i][j] = max(1,val);
+            }
+        }
+        return dp[0][0];
+    }
+};
